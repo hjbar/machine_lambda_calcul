b7d101d60cf1bca0f6b976f0e7ca3c78d5c6b03a
hjbar <hjbar@protonmail.com>
2024-05-15 19:16:42 +0200

    Structure + readability

--- weak_cbn/bin/main.ml
+++ weak_cbn/bin/main.ml
@@ -1,15 +1,30 @@
 open Lambda
+
+let print_reduc t1 t2 =
+  pp_lambda t1;
+  Format.printf " -> ";
+  pp_lambda t2
+
+(* Test pour naif_interp *)
+
 open Naif_interp
 
-let t1 = Abs ("x", Var "x")
-
-let omega =
-  let delta = Abs ("x", App (Var "x", Var "x")) in
-  App (delta, delta)
+let () = Format.printf "Naif_interp :\n%!"
 
 let () =
-  if fst @@ eval t1 <> t1 then failwith "Error with t1 in weak_cbn";
+  let t = Abs ("x", Var "x") in
+  let res = eval t |> fst in
+  if t <> res then failwith "Error with test1 in weak_cbn_naif"
+  else print_reduc t res;
+  Format.print_newline ()
 
-  fst @@ eval omega |> pp_lambda;
+(*
+let () =
+  let omega =
+    let delta = Abs ("x", App (Var "x", Var "x")) in
+    App (delta, delta)
+  in
+  eval omega |> fst |> pp_lambda
+*)
 
-  Printf.printf "weak_cbn : OK\n%!"
+let () = Format.printf "weak_cbn : OK\n%!"
--- weak_cbn/bin/naif_interp.ml
+++ weak_cbn/bin/naif_interp.ml
@@ -6,18 +6,18 @@ type env = Env of (lambda_term * env) StringMap.t
 let get_env e = match e with Env e -> e
 
 let eval t =
-  let rec eval t env =
+  let rec eval t e =
     match t with
     | Var x ->
-      let e = get_env env in
+      let e = get_env e in
       let t', e' = StringMap.find x e in
       eval t' e'
-    | Abs _ as res -> (res, env)
+    | Abs _ as res -> (res, e)
     | App (t1, t2) -> begin
-      match eval t1 env with
-      | Abs (x, t'), e ->
-        let e = get_env e in
-        let e' = StringMap.add x (t2, env) e in
+      match eval t1 e with
+      | Abs (x, t'), e' ->
+        let e' = get_env e' in
+        let e' = StringMap.add x (t2, e) e' in
         eval t' (Env e')
       | _ -> assert false
     end
