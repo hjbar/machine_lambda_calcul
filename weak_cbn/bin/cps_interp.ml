b5361190a04f8b5661725480634f5363745fb7cb
hjbar <hjbar@protonmail.com>
2024-05-16 09:21:13 +0200

    Add cps version of weak_cbn

--- weak_cbn/bin/cps_interp.ml
+++ weak_cbn/bin/cps_interp.ml
@@ -6,20 +6,21 @@ type env = Env of (lambda_term * env) StringMap.t
 let get_env e = match e with Env e -> e
 
 let eval t =
-  let rec eval t e =
+  let rec eval t e k =
     match t with
     | Var x ->
       let e = get_env e in
       let t', e' = StringMap.find x e in
-      eval t' e'
-    | Abs _ -> (t, e)
+      eval t' e' k
+    | Abs _ -> k t e
     | App (t1, t2) -> begin
-      match eval t1 e with
-      | Abs (x, t'), e' ->
+      eval t1 e @@ fun t1' e' ->
+      match t1' with
+      | Abs (x, t') ->
         let e' = get_env e' in
         let e' = StringMap.add x (t2, e) e' in
-        eval t' (Env e')
+        eval t' (Env e') k
       | _ -> assert false
     end
   in
-  eval t (Env StringMap.empty)
+  eval t (Env StringMap.empty) (fun t e -> (t, e))
--- weak_cbn/bin/main.ml
+++ weak_cbn/bin/main.ml
@@ -5,6 +5,8 @@ let print_reduc t1 t2 =
   Format.printf " -> ";
   pp_lambda t2
 
+let () = Format.print_newline ()
+
 (* Test pour naif_interp *)
 
 open Naif_interp
@@ -24,7 +26,37 @@ let () =
     let delta = Abs ("x", App (Var "x", Var "x")) in
     App (delta, delta)
   in
-  eval omega |> fst |> pp_lambda
+  eval omega |> fst |> pp_lambda;
+  Format.print_newline ()
 *)
 
-let () = Format.printf "weak_cbn : OK\n%!"
+let () = Format.printf "weak_cbn_naif : OK\n%!"
+
+let () = Format.print_newline ()
+
+(* Test pour cps_interp *)
+
+open Cps_interp
+
+let () = Format.printf "Cps_interp :\n%!"
+
+let () =
+  let t = Abs ("x", Var "x") in
+  let res = eval t |> fst in
+  if t <> res then failwith "Error with test1 in weak_cbn_naif"
+  else print_reduc t res;
+  Format.print_newline ()
+
+(*
+let () =
+  let omega =
+    let delta = Abs ("x", App (Var "x", Var "x")) in
+    App (delta, delta)
+  in
+  eval omega |> fst |> pp_lambda;
+  Format.print_newline ()
+*)
+
+let () = Format.printf "weak_cbn_cps : OK\n%!"
+
+let () = Format.print_newline ()
