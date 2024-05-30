3d1ce8d635ab234bb6d96295278f59bf11cf4cda
hjbar <hjbar@protonmail.com>
2024-05-30 14:19:14 +0200

    Rewrite some stuff

--- lib/cps/cps.ml
+++ lib/cps/cps.ml
@@ -3,5 +3,5 @@ let rec map (f : 'a -> ('b -> 'c) -> 'c) (l : 'a list) (k : 'b list -> 'c) : 'c
   match l with
   | [] -> k []
   | x :: l' ->
-    f x @@ fun y ->
-    map f l' @@ fun l'' -> k @@ (y :: l'')
+    f x @@ fun x' ->
+    map f l' @@ fun l'' -> k @@ (x' :: l'')
