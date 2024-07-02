3a84e461802902e0d8180c767321bf18852fa3ac
hjbar <hjbar@protonmail.com>
2024-07-02 17:32:48 +0200

    Remove useless cps lib

--- lib/cps/cps.ml
+++
@@ -1,6 +0,0 @@
-let rec map (f : 'a -> ('b -> 'c) -> 'c) (l : 'a list) (k : 'b list -> 'c) : 'c =
-  match l with
-  | [] -> k []
-  | x :: l' ->
-    f x @@ fun x' ->
-    map f l' @@ fun l'' -> k @@ (x' :: l'')
